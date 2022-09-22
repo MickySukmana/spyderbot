27fc16976ec28c087b3720a8b40ac7af8ef35ec1
Micky <hibatullah.mickysukmana@gmail.com>
2022-09-22 10:25:31 +0700

    added walking motion

--- src/walk.py
+++ src/walk.py
@@ -10,19 +10,30 @@ from time import sleep
 class walk():
     def __init__(self):
         ik = inverse_kinematic(40, 40)
+        swing = 5
+        drop = 20
 
-        self.fs = ik.calc(30, 40, 5)	#move front leg 10cm forward
-        self.fd = ik.calc(30, 40, 20)	#drop front leg
-        self.rs = ik.calc(30, 20, 5)	#move rear leg 10cm forward
-        self.rd = ik.calc(30, 20, 20)	#drop rear leg
-        self.ip = ik.calc(30, 30, 20)	#return to initial position
+        self.fs = ik.calc(30, 40, swing)	#move front leg 10cm forward
+        self.fd = ik.calc(30, 40, drop)		#drop front leg
+        self.rs = ik.calc(30, 20, swing)	#move rear leg 10cm forward
+        self.rd = ik.calc(30, 20, drop)		#drop rear leg
+        self.ip = ik.calc(30, 30, drop)		#return to initial position
+        self.ll = ik.calc(30, 50, drop)
+        self.ips = ik.calc(30, 30, swing)
 
     def forward(self):
         movement = [
-            self.ip + self.ip + self.ip + self.ip,
+            self.ip + self.rd + self.ip + self.ip,
             self.ip + self.fs + self.ip + self.ip,
-            self.ip + self.fd + self.rs + self.ip,
-            self.ip + self.fd + self.rd + self.ip,
+            self.ip + self.fd + self.ip + self.ip,
+            self.fd + self.ip + self.fd + self.rd,
+            self.fd + self.ip + self.rs + self.rd,
+            self.fd + self.ip + self.rd + self.rd,
+            self.fd + self.ip + self.rd + self.fs,
+            self.fd + self.ip + self.rd + self.fd,
+            self.ll + self.rd + self.ip + self.ip,
+            self.ips + self.rd + self.ip + self.ip,
+            self.ip + self.rd + self.ip + self.ip
         ]
         
         for angle in movement:   
